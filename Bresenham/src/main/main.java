af53781413045ac51e5283c5678fd5208319d1c1
leenahere <lina.pte@googlemail.com>
2015-05-21 13:50:44 +0200

    neues syso

--- Bresenham/src/main/main.java
+++ Bresenham/src/main/main.java
@@ -6,6 +6,7 @@ public class main {
     // TODO Auto-generated method stub
     System.out.println("Hallo Cogra-Welt!");
     System.out.println("Guten Morgen Fabian");
+    System.out.println("Hallo Leute!");
 
   }
 
