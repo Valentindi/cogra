3f69d18768dee6bf74e5fbde87c59d06d5436882
Valentin Dittmar <valentin.dittmar@stud.htwk-leipzig.de>
2015-05-21 14:51:33 +0200

    [Add] Scene ist jetzt zu sehen

--- Bresenham/src/gui/GuiView.java
+++ Bresenham/src/gui/GuiView.java
@@ -1,5 +1,35 @@
 package gui;
 
-public class GuiView {
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
+public class GuiView {
+	
+	private AnchorPane rootLayout;
+
+	public void initGuiView(Stage primaryStage) {
+		
+		try {
+			System.out.println("TryInit");
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(GuiView.class.getResource("GuiFX.fxml"));
+			rootLayout = (AnchorPane)  loader.load();
+			
+			
+			System.out.println(rootLayout.toString());
+			Scene scene = new Scene(rootLayout);
+			System.out.println(scene.toString());
+			
+			
+			primaryStage.setScene(scene);
+			primaryStage.show();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
 }
--- Bresenham/src/main/main.java
+++ Bresenham/src/main/main.java
@@ -1,13 +1,34 @@
 package main;
 
-public class main {
+import javafx.application.Application;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+import gui.GuiView;
 
+public class main extends Application {
+
+	private Stage primaryStage;
+	
   public static void main(String[] args) {
     // TODO Auto-generated method stub
     System.out.println("Hallo Cogra-Welt!");
     System.out.println("Guten Morgen Fabian");
     System.out.println("Hallo Leute!");
+    
+    launch(args);
 
   }
+
+
+
+@Override
+public void start(Stage primaryStage) throws Exception {
+
+	this.primaryStage = primaryStage;
+	//this.primaryStage.setTitle("AntiAliasing und Bresenham");
+	
+	GuiView guiView = new GuiView();
+	guiView.initGuiView(primaryStage);
+}
 
 }
